2a6afc8630595cd512a2b2c6c0ed9756694f2350
Kevin <quotenslav@gmail.com>
2023-04-02 13:31:34 +0200

    Added Reqs to README and changed enum piece count

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 Future Chess Application that you can play against an AI that should use min-max algorithmen in the future. Possible variation would be to make the app work as a CLI Application, for learning to play blind.
 
 The main goal is to learn basic python
+
+## Requierments:
+
+- [] Generate board
+- [] Move Pieces
+- [] Check for legal moves
+- [] Check for Check/Checkmate/Draw/Stalemate
+- [] Add special Chess Moves (Castling, En Passant)
+- [] Add promotion
+- [] Track moves
+- [] Count worth of pawns and pieces
--- index.py
+++ index.py
@@ -4,13 +4,23 @@ def main():
     print("Hello World!")
     blackKing = Piece(Colour.BLACK, "h4", PieceType.KING)
     blackKing.getInfo()
+    blackKnight = Piece(Colour.BLACK, "h4", PieceType.KNIGHT)
+    blackKnight.getInfo()
+    blackBishop = Piece(Colour.BLACK, "h4", PieceType.BISHOP)
+    blackBishop.getInfo()
+    startGame()
+
+def startGame():
+    pass
+
+def checkAllowedMoves(Piece):
+    pass
 
 class Piece:
-    def __init__(self, Colour, Position, PieceType):
-        self.Colour = Colour
-        self.Position = Position
-        self.PieceType = PieceType
-        pass
+    def __init__(self, colour, position, pieceType):
+        self.Colour = colour
+        self.Position = position
+        self.PieceType = pieceType
 
     def getColour(self):
         return self.Colour.name
@@ -19,7 +29,7 @@ class Piece:
     def getType(self):
         return self.PieceType.name
     def getInfo(self):
-        print(str(self.Colour.name) + " " + str(self.PieceType.name))
+        print(str(self.Colour.name) + " " + str(self.PieceType.name) + " has a value of: " + str(self.PieceType.value))
 
 
 class Colour(Enum):
@@ -27,12 +37,12 @@ class Colour(Enum):
     BLACK = 1
 
 class PieceType(Enum):
-    KING = 0
-    QUEEN = 1
-    ROOK = 2
-    BISHOP = 3
-    HORSE = 4
-    PAWN = 5
+    KING = 100
+    QUEEN = 9
+    ROOK = 5
+    BISHOP = 3 # This may need to be changed in the future because it is not proper use of enumeration
+    KNIGHT = 3
+    PAWN = 1
 
 if __name__ == "__main__":
     main()
